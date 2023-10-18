d53a549ae5b7d17ea76b59fec0f81e202e54de4d
Vishal <vishal@localhost.localdomain>
2023-10-18 10:58:01 +0530

    first code

---
+++ page.js
@@ -0,0 +1,318 @@
+var btn1 = document.getElementById("subject");
+var unit = document.getElementById('unit');
+var btn = document.querySelector(".btn2");
+
+btn.addEventListener("click", function () {
+    // console.log("heooo");
+    if (btn1.value === "BAS201") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1hnSbOSqnoap0aDg4retabM1vGOvB_mds/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://drive.google.com/file/d/1bEDSiE5V7odSf9PWuprwCUo_l7K_XbI9/view')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg');
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg');
+        }
+    }
+
+    // sec sub bas202 chemistry.
+    else if (btn1.value === "BAS202") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1o_oKjH6sY9JHrRDnvUlBaIsM7TVRUazy/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://drive.google.com/file/d/1xDngjKGnoffQYnS9ctwiXhL9_DrzWgP-/view')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+
+    //  for bas203 maths-2
+    else if (btn1.value === "BAS203") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1joIIJOqlsnarDiKZxTe7qhfRgnK90yk-/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+    // for Environment bas204
+    else if (btn1.value === "BAS204") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/169D24O4dJNC3SHwcqUkblsPAFHrOmRlo/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+
+    // for soft skills bas205
+    else if (btn1.value === "BAS205") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1eNk5AZa3TTtbpPzLuAc8fHLEOySrPWyo/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+    // for C LANG BCS 201
+    else if (btn1.value === "BCS201") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1VxvRXz1F_fYCWDVqBxHALeBlOYBZyriG/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+    // for ELECTRONIC LANG BEC 201
+    else if (btn1.value === "BEC201") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1vpALUeyVBhVobwb2EG4BuoQWs9GV8R3a/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+    // for electrical
+    else if (btn1.value === "BEE201") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1Ejo96GDNPCO3I9KTTgwOsfGIOEo9qWAn/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+    // FOR MECANICAL BME
+    else if (btn1.value === "BME201") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1_E9ci4hYuV0fVPUnUYgHD6SUfD1_dm24/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+     // FOR COA 
+     else if (btn1.value === "BCS302") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1EiJXW5f-GVQpnJjX5yxNxW_HR87DyvPI/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+    // FOR DS BCS301
+    else if (btn1.value === "BCS301") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1WN8n6fUEqp7pqba93vU39MKQlxUDwEDr/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+     // FOR DS BCS301
+     else if (btn1.value === "BCS301") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1WN8n6fUEqp7pqba93vU39MKQlxUDwEDr/view");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    } // FOR DSTL BCS303
+    else if (btn1.value === "BCS303") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1O99xu0sDLjxsiaKqZYDcHsooGaspFF_j/view?usp=sharing");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+    // FOR MATHS-4
+    else if (btn1.value === "BAS303") {
+        if (unit.value === "1") {
+            window.open("https://drive.google.com/file/d/1V7KZdUpM9Ub90kRgEtTib_2SHH7cviG_/view?usp=sharing");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+    // FOR TC  BAS301
+    else if (btn1.value === "BAS301") {
+        if (unit.value === "1") {
+            window.open("https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+     // FOR CS  BCC301
+     else if (btn1.value === "BCC301") {
+        if (unit.value === "1") {
+            window.open("https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg");
+        }
+        else if (unit.value === "2") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+        else if (unit.value === "3") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "4") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        } else if (unit.value === "5") {
+            window.open('https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg')
+        }
+    }
+});
+
+
+// for secound year subs 
+let isFirstYear = true; // Variable to track if it's the first year or second year
+
+document.getElementById('changeSubjectBtn').addEventListener('click', function () {
+    const subjectDropdown = document.getElementById('subject');
+
+    if (isFirstYear) {
+        // Switch to second-year subjects
+        document.querySelector('span').innerHTML = 'Second Year &#8595;:-';
+        subjectDropdown.innerHTML = ''; // Clear the existing options
+        const subjects = [
+            { value: "_BLANK", text: "SELECT" },
+            { value: "BCS301", text: "BCS301(DS)" },
+            { value: "BCS302", text: "BCS302 (COA)" },
+            { value: "BCS303", text: "BCS303 (DSTL)" },
+            { value: "BAS303", text: "BAS303 (MATHS-4 (QUANTUM ONLY))" },
+            { value: "BAS301", text: "BAS301 (TC)" },
+            { value: "BCC301", text: "BCC301 (CS)" },
+            // Add your second-year subjects here
+        ];
+        subjects.forEach(subject => {
+            const option = document.createElement('option');
+            option.value = subject.value;
+            option.textContent = subject.text;
+            subjectDropdown.appendChild(option);
+        });
+    } else {
+        // Switch back to first-year subjects
+        document.querySelector('span').innerHTML = 'First Year &#8595;:-';
+        subjectDropdown.innerHTML = ''; // Clear the existing options
+        const subjects = [
+            { value: "_BLANCK", text: "SELECT" },
+            { value: "BAS201", text: "BAS201 (Physics)" },
+            { value: "BAS202", text: "BAS202 (Chemistry)" },
+            { value: "BAS203", text: "BAS203 (Maths-2)" },
+            { value: "BAS204", text: "BAS204 (Environment)" },
+            { value: "BAS205", text: "BAS205 (SOFT SKILLS)" },
+            { value: "BCS201", text: "BCS201 (C LANG.)" },
+            { value: "BEC201", text: "BEC201 (ELECTRONIC)" },
+            { value: "BEE201", text: "BEE201 (ELECTRICAL)" },
+            { value: "BME201", text: "BME201 (MECHANICAL)" },
+        ];
+        subjects.forEach(subject => {
+            const option = document.createElement('option');
+            option.value = subject.value;
+            option.textContent = subject.text;
+            subjectDropdown.appendChild(option);
+        });
+    }
+
+    isFirstYear = !isFirstYear; // Toggle between first-year and second-year
+});
